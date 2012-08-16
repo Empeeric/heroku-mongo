0a53e58f3cc0fe19a76aba4548b8e8ca47943614
John Simone <john@heroku.com>
2012-08-16 14:05:05 -0700

    add contributor to app command

--- lib/heroku/command/manager.rb
+++ lib/heroku/command/manager.rb
@@ -16,6 +16,8 @@ class Heroku::Command::Manager < Heroku::Command::BaseWithApp
     display "heroku manager:apps --org ORG_NAME"
     display "heroku manager:transfer (--to|--from) ORG_NAME [--app APP_NAME]"
     display "heroku manager:add_user --org ORG_NAME --user USER_EMAIL --role ROLE"
+    display "heroku manager:add_contributor_to_app --org ORG_NAME --user USER_EMAIL [--app APP_NAME]"
+    display "Heroku Teams Migration Commands:"
     display "heroku manager:team_to_org --team TEAM_NAME --org ORG_NAME"
     display "heroku manager:org_to_team --org ORG_NAME --team TEAM_NAME"
 
@@ -209,7 +211,7 @@ class Heroku::Command::Manager < Heroku::Command::BaseWithApp
   # -u, --user USER_EMAIL     # User to add
   # -o, --org ORG       # org the app is in
   #
-  def add_user
+  def add_contributor_to_app
     user = options[:user]
     org = options[:org]
 
@@ -236,9 +238,7 @@ class Heroku::Command::Manager < Heroku::Command::BaseWithApp
         print_and_flush("failed\nAn error occurred: #{response.code}\n#{response}\n")
       end
     rescue => e
-      if e.response && e.response.code == 302
-        print_and_flush("failed\n#{user} already has access to #{app}\n")
-      elsif e.response
+      if e.response
         errorText = json_decode(e.response.body)
         print_and_flush("failed\nAn error occurred: #{errorText["error_message"]}\n")
       else
