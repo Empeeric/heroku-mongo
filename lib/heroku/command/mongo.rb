4436ab2ef14e045eef9d19139d70c3b09cba4c29
Refael Ackermann <refael@empeeric.com>
2013-03-07 20:29:32 +0200

    added restore

--- lib/heroku/command/mongo.rb
+++ lib/heroku/command/mongo.rb
@@ -43,4 +43,29 @@ class Heroku::Command::Mongo < Heroku::Command::BaseWithApp
         exec(cmd)
     end
 
+    def restore
+        @app = shift_argument || options[:app] || options[:confirm]
+        validate_arguments!
+
+        unless @app
+          error("Usage: heroku mongo:restore --app APP\nMust specify APP to restore to.")
+        end
+
+        api.get_app(@app) # fail fast if no access or doesn't exist
+
+        message = "WARNING: Potentially Destructive Action\nThis command will destroy data for #{@app} ."
+        if confirm_command(@app, message)
+            action("Restoring #{@app}") do
+                key = "MONGOLAB_URI"
+                vars = api.get_config_vars(app).body
+                uri = URI(vars[key])
+                hash = {}
+                uri.instance_variables.each {|var| hash[var.to_s.delete("@").to_sym] = uri.instance_variable_get(var) }
+                hash[:db] = hash[:path][1..-1]
+                cmd = "mongorestore --drop -u %{user} -p %{password} -d %{db} -h %{host}:%{port} dump/%{db}" % hash
+                exec(cmd)
+            end
+        end
+    end
+
 end
