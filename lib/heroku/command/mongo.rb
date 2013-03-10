e24a234f29811dcb979d3e1c0657c6e8e8d57cc9
Refael Ackermann <refael@empeeric.com>
2013-03-10 13:00:36 +0200

    Now with load to local action

--- lib/heroku/command/mongo.rb
+++ lib/heroku/command/mongo.rb
@@ -1,23 +1,33 @@
-require 'heroku/command/base'
-require 'rest_client'
-require 'date'
-
-# manage apps in organization accounts
-#
-
-require 'uri'
-require 'json'
+require "heroku/command/base"
+require "uri"
 
 module URI
-  class MONGODB < Generic
-    DEFAULT_PORT = 27017
-  end
-  @@schemes['MONGODB'] = MONGODB
+    class MONGODB < Generic
+        DEFAULT_PORT = 27017
+    end
+    @@schemes['MONGODB'] = MONGODB
 end
 
-class Heroku::Command::Mongo < Heroku::Command::BaseWithApp
 
+# manage some MongoDBaaS
+#
+class Heroku::Command::Mongo < Heroku::Command::Base
+
+    # mongo
+    #
+    # manage some MongoDBaaS
+    #
+    # -d, --dbname  # name of the local DB
+    #
     def index
+      validate_arguments!
+
+      cmd = "mongo -u %{user} -p %{password} %{host}:%{port}%{path}" % hash
+      exec(cmd)
+    end
+
+
+    def console
         validate_arguments!
 
         key = "MONGOLAB_URI"
@@ -29,8 +39,14 @@ class Heroku::Command::Mongo < Heroku::Command::BaseWithApp
         exec(cmd)
     end
 
-    
+
+    # mongo:dump
+    #
+    # dump the remote db and load it to local DBNAME
+    #
+    # -d, --dbname DBNAME  # load it to local DBNAME
     def dump
+        dbname = shift_argument
         validate_arguments!
 
         key = "MONGOLAB_URI"
@@ -41,12 +57,37 @@ class Heroku::Command::Mongo < Heroku::Command::BaseWithApp
         hash[:db] = hash[:path][1..-1]
         cmd = "mongodump -u %{user} -p %{password} -d %{db} -h %{host}:%{port}" % hash
         exec(cmd)
+        if dbname
+            cmd1 = "mongorestore --drop -d %{dbname} dump/%{db}" % hash
+            exec(cmd1)
+        end
     end
 
+    
+    # mongo:load
+    #
+    # load it to local DBNAME
+    #
+    # -d, --dbname DBNAME  # load it to local DBNAME
+    def load
+        validate_arguments!
+
+        key = "MONGOLAB_URI"
+        vars = api.get_config_vars(app).body
+        uri = URI(vars[key])
+        hash = {}
+        uri.instance_variables.each {|var| hash[var.to_s.delete("@").to_sym] = uri.instance_variable_get(var) }
+        hash[:db] = hash[:path][1..-1]
+        hash[:dbname] = options[:dbname]
+        cmd1 = "mongorestore --drop -d %{dbname} dump/%{db}" % hash
+        exec(cmd1)
+    end
+
+    
+    
     def restore
         @app = shift_argument || options[:app] || options[:confirm]
         validate_arguments!
-
         unless @app
           error("Usage: heroku mongo:restore --app APP\nMust specify APP to restore to.")
         end
